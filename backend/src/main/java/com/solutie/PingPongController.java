a44bb4f93ca636c34031ab5f1cad986aaa5320de
fred <fred.odhis@gmail.com>
2023-04-19 05:15:16 +0300

    update counter on pingpong controller

--- backend/src/main/java/com/solutie/PingPongController.java
+++ backend/src/main/java/com/solutie/PingPongController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PingPongController {
+
+    private static int COUNTER = 0;
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("Pongi");
+        return new PingPong("Pongi %S".formatted(++COUNTER));
     }
 
 }
